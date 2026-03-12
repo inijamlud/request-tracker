bf58ca517c52d3ad99403c99e67670ccb1f06260
inijamlud <jamalude449@gmail.com>
2026-03-12 10:35:59 +0700

    first init

---
+++ app/requests/page.tsx
@@ -0,0 +1,8 @@
+export default function RequestPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Requests</h1>
+      <p>List of client requests</p>
+    </div>
+  );
+}
